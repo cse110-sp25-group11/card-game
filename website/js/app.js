404a844ff9535c4248d4f14e94338b049302459f
Sachin Ramanathan <sachinr.exe@gmail.com>
2025-05-11 13:13:20 -0700

    Add website template

---
+++ website/js/app.js
@@ -0,0 +1,9 @@
+/**
+ * Show an alert when the button is clicked.
+ * @returns {void}
+ */
+function handleClick() {
+  alert('Button clicked!');
+}
+
+document.getElementById('btn').addEventListener('click', handleClick);
