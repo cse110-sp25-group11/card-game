5208350b8a3de8e476e1f048cbc9adcc9b626621
benjamin miller <bem002@ucsd.edu>
2025-05-11 21:24:18 -0700

    added sum function

--- __tests__/pseudo.tests.js
+++ __tests__/pseudo.tests.js
@@ -1,16 +1,14 @@
-import {sum} from "./sum_function.js"
+import { sum } from "./sum_function.js";
 
 /**
  * Testing function: {function name}
  */
 
 // true tests
-test('Checks the sum function for 1 + 2', () => {
-    expect(sum(1, 2)).toBe(3);  // wrong test
+test("Checks the sum function for 1 + 2", () => {
+  expect(sum(1, 2)).toBe(3);
 });
 
-test('Checks the sum function -3 + -5', () => {
-    expect(sum(-3, -5)).toBe(-8);
+test("Checks the sum function -3 + -5", () => {
+  expect(sum(-3, -5)).toBe(-8);
 });
-
-// false tests (will be added with more functions)
