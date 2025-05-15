e5c91250ddafdf3efe4b16626c4004bccecff80e
Sachin Ramanathan <61686882+Sachin-dot-py@users.noreply.github.com>
2025-05-15 15:14:03 -0700

    Implement Delete Local Storage

---
+++ website/js/storage.js
@@ -0,0 +1,9 @@
+/**
+ * Clears all key/value pairs from the browser's localStorage.
+ *
+ * @function deleteLocalStorage
+ * @returns {void}
+ */
+function deleteLocalStorage(){
+    localStorage.clear();
+}
