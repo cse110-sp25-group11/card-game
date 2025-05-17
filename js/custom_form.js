59f92136703e0b7196dcfb7a0a41d64f8bffb3d2
pycoder49 <pycoder49@users.noreply.github.com>
2025-05-17 07:25:23 +0000

    deploy: 82f09368d242696b613ea3b47e7da49d7aca6ec9

---
+++ js/custom_form.js
@@ -0,0 +1,39 @@
+window.addEventListener("DOMContentLoaded", () =>{
+    const form = document.getElementById("custom-form");
+    const food_radios = Array.from(form.elements["food"]); // converts to array
+    const food_details = document.getElementById("food-details");
+
+    food_radios.forEach((radio) => {
+    radio.addEventListener("change", () => {
+        food_details.style.display = radio.value === "Yes" && radio.checked ? "block" : "none";
+        });
+    });
+
+    form.addEventListener("submit", (e) => {
+        e.preventDefault();     // this prevents page reload during submission
+
+        // creating a eventData object to be stored in local storage
+        const eventData = {
+            event_name: form.elements["event-name"].value,
+            org_name: form.elements["org-name"].value,
+            date: form.elements["event-date"].value,
+            duration: form.elements["duration"].value,
+            location: form.elements["location"].value,
+            short_description: form.elements["short-description"].value,
+            long_description: form.elements["long-description"].value,
+            food: form.elements["food"].value,
+            foodDetails: form.elements["food-details"].value,
+        }
+
+        // storing all this in local storage
+        localStorage.setItem("postedEvent", JSON.stringify(eventData));
+
+        // post submission
+        alert("Event Submitted!");
+        form.reset();   // resets the form
+        food_radios.forEach(r => r.checked = false);    // unchecks all radios
+        food_details.style.display = "none";        // hides the food-details box
+    });
+
+    
+});
