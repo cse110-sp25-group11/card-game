87ab9a733ab5ef911d296bc3f490e49509dd737b
miachen67 <miachen2005@gmail.com>
2025-05-17 22:55:43 -0700

    html for event-card component completed

--- website/js/app.js
+++
@@ -1,9 +0,0 @@
-/**
- * Show an alert when the button is clicked.
- * @returns {void}
- */
-function handleClick() {
-  alert('Button clicked!');
-}
-
-document.getElementById('btn').addEventListener('click', handleClick);
