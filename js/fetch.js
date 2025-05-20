8f23b6f2d9ebf3ba6270ecf3a2be06bb5346ae0a
BenMiller0 <BenMiller0@users.noreply.github.com>
2025-05-20 00:50:14 +0000

    deploy: 70a195a10fa2c5ad1f270a2b6f5b2453331debe7

---
+++ js/fetch.js
@@ -0,0 +1,17 @@
+/*  Fetches data from local storageg and verifies that it is a valid JSON
+ *  @param {data}
+ *  @returns {int}
+ */
+function fetchData(data) {
+    if(!data){
+        console.warn(`[!] Failed to parse "${data}"`);
+        return [];
+    }
+    
+	const rawdata = localStorage.getItem(data);
+	if (!rawdata){
+        console.warn(`[!] Failed to parse "${data}"`);
+        return [];
+    } 
+    return JSON.parse(rawdata);
+}
