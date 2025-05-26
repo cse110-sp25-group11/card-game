1c3090ddb49000f45bf6d29512c0cc21dfaff3d2
BenMiller0 <bem002@ucsd.edu>
2025-05-26 13:31:25 -0700

    added checkvalidevent tests

--- __tests__/pseudo.tests.js
+++ __tests__/pseudo.tests.js
@@ -1,4 +1,5 @@
 import { sum } from "../website/js/sum_function.js";
+import { checkValidEvent } from "../website/js/app.js";
 
 /**
  * Testing function: {function name}
@@ -12,3 +13,26 @@ test("Checks the sum function for 1 + 2", () => {
 test("Checks the sum function -3 + -5", () => {
   expect(sum(-3, -5)).toBe(-8);
 });
+
+
+
+/**
+ * Testing function: {checkValidEvent}
+ */
+const validEvent = {
+    name: "Team 11 Meeting",
+    description: "team 11 standup meeting",
+    date: "2025-21-2025",
+    org: "Team 11",
+    imgLink: "https://team11.com/image.jpg",
+    imgAltText: "Tech Talk Banner",
+    location: "CSE Basement",
+    food: false,
+    startTime: "14:00",
+    endTime: "16:00",
+}
+
+test('returns true for a valid event', () => {
+    expect(checkValidEvent(validEvent)).toBe(true);
+});
+
