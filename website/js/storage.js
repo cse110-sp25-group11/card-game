eb42ecb49b7776b607776870ade018fa723392ef
BenMiller0 <bem002@ucsd.edu>
2025-05-25 20:43:45 -0700

    added comment to storage.js

--- website/js/storage.js
+++ website/js/storage.js
@@ -41,5 +41,5 @@ function fetchData(data) {
     console.warn(`[!] Failed to parse "${data}"`);
     return [];
   }
-  return JSON.parse(rawdata);
+  return JSON.parse(rawdata); // Return raw data parsed 
 }
