e31e064c2b75a0abd3f111d3b45670a56f0ef45b
BenMiller0 <bem002@ucsd.edu>
2025-06-08 00:05:33 -0700

    finished storage tests

--- __tests__/stoage.test.js
+++ __tests__/stoage.test.js
@@ -1,9 +1,29 @@
 import { fetchData, addToStorage } from "../website/js/storage.js";
 
-test("addToStorage rejects bad data", () => {
+// clearing local storage
+beforeEach(() => {
+    localStorage.clear();
+});
+
+test("fetchData rejects bad data", () => {
     expect(fetchData(null)).toStrictEqual([]);
     expect(fetchData()).toStrictEqual([]);
     expect(fetchData("junk")).toStrictEqual([]);
+});
+
+// clearing local storage
+beforeEach(() => {
+    localStorage.clear();
+});
+
+test("fetchData accepts good data", () => {
+    expect(addToStorage("5","5")).toStrictEqual(true);
+    expect(fetchData("5")).toStrictEqual(5);
+});
+
+// clearing local storage
+beforeEach(() => {
+    localStorage.clear();
 });
 
 test("add data with key twice, should be false second time", () =>{
